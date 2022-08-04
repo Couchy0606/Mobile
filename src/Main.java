fe69efdcaf9633b8c19af7aef172b2e4d18a4644
Couchy0606 <irchik669@gmail.com>
2022-08-04 20:28:20 +0300

    changes modified

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        int amount = 200; // сумма на счету
+        int addamount = 1300; // сумма пополнения
+        int bonus = 100; // бонусный рубль за 100 рублей
+
+        int totalbonus;
+        if (addamount > 1000) {
+            totalbonus = addamount / bonus;
+        } else {
+            totalbonus = 0;
+        }
+
+        // int totalbonus = addamount > 1000 ? addamount / bonus : 0;
+
+        int totalamount = amount + totalbonus + addamount;
+
+        System.out.println("Сумма бонуса: " + totalbonus);
+        System.out.println("Итого на счету: " + totalamount);
+    }
+
 }
+
